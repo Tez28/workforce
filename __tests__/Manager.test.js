373c7322d73eecf05b3d7eede1447c0a1a96f0fa
Tez28 <courtezcannady@gmail.com>
2022-02-27 11:51:07 -0500

    generateHTML and fix manager test

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,15 +1,13 @@
 // add manager constructor
-const { expect } = require('@jest/globals');
-const { test } = require('picomatch');
 const Manager = require('../lib/Manager');
 
 // add manager construct
-test('create Manager object', () => {
+test('creates Manager object', () => {
     const manager = new Manager('Courtez', 2, 'courtezcannady@gmail.com', 10);
     expect(manager.office).toEqual(expect.any(Number));
 });
 
-// adds roles
+// adds employee role
 test('add role for employee', () => {
     const manager = new Manager('Courtez', 2, 'courtezcannady@gmail.com', 10);
     expect(manager.getJob()).toEqual("Manager");
---
+++ src/generateHTML.js
@@ -0,0 +1,133 @@
+//creates card for manager
+const generateManager = function (manager) {
+    return `
+    <div class="col-4 mt-4">
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${manager.name}</h3>
+                <h4>Manager</h4><i class="material-icons">content_paste</i>
+            </div>
+
+            <div class="card-body">
+                <p class="id">ID: ${manager.id}</p>
+                <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
+                <p class="office">Office Number: ${manager.office}</p>
+            </div>
+
+        </div>
+    </div>  
+    `;
+}
+
+// create Engineer card
+const generateEngineer = function (engineer) {
+    return `
+    <div class="col-4 mt-4">
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${engineer.name}</h3>
+                <h4>Engineer</h4><i class="material-icons">laptop_mac</i>
+            </div>
+
+            <div class="card-body">
+                <p class="id">ID: ${engineer.id}</p>
+                <p class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+                <p class="github">Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
+            </div>
+
+        </div>
+    </div>
+    `
+}
+
+// create Intern card 
+const generateIntern = function (intern) {
+    return `
+    <div class="col-4 mt-4">
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${intern.name}</h3>
+                <h4>Intern</h4><i class="material-icons">assignment_ind</i>
+            </div>
+
+            <div class="card-body">
+                <p class="id">ID: ${intern.id}</p>
+                <p class="email">Email:<a href="mailto:${intern.email}">${intern.email}</a></p>
+                <p class="school">School: ${intern.university}</p>
+            </div>
+    </div>
+</div>
+    `
+};
+
+//push htnl to page
+generateHTML = (data) => {
+    cardArray = [];
+
+    for (let i = 0; i < data.length; i++) {
+        const employee = data[i];
+        const job = employee.getJob();
+
+        // calls manager up
+        if (job === 'Manager') {
+            const managerCard = generateManager(employee);
+            cardArray.push(managerCard);
+        }
+
+        // calls engineer up
+        if (job === 'Engineer') {
+            const engineerCard = generateEngineer(employee);
+            cardArray.push(engineerCard);
+        }
+        // calls up intern card
+        if (job === 'Intern') {
+            const internCard = generateIntern(employee);
+            cardArray.push(internCard);
+        }
+    }
+    const employeeCard = cardArray.join('')
+    const generateWorkers = generateWorkForce(employeeCard);
+    return generateWorkers;
+}
+
+// call to generate formatted page
+const generateTeamPage = function (employeeCard) {   
+    return`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>My Team</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+        <link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@600&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header>
+            <nav class="navbar" id="navbar">
+                <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">Team Profile</span>
+            </nav>
+        </header>
+        <main>
+            <div class="container">
+                <div class="row justify-content-center" id="team-cards">
+                    <!--Team Cards-->
+                    ${employeeCard}
+                </div>
+            </div>
+        </main>
+        
+    </body>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
+    </html>
+  
+  
+  `;
+  }
+// module.export
+module.exports = generateHTML;
