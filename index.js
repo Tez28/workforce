37668eaecb7707dee806d92ed436435d147f4f57
Tez28 <courtezcannady@gmail.com>
2022-02-27 10:35:02 -0500

    add manager input

--- index.js
+++ index.js
@@ -12,3 +12,67 @@ const Intern = require('./lib/Intern');
 const generateHTML = require('.src/generateHTML');
 // create team array
 const myTeam = [];
+
+// manager prompts
+const newManager = () => {
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the team Managers name? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log ("Please enter the managers name!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Please enter the managers ID. (Required)',
+            validate: idInput => {
+                if (idInput) {
+                    return true;
+                } else {
+                    console.log ("Please provide the managers ID!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the managers email address? (Required)',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log ('"Please input an email address for the manager!');
+                    return false;
+                } 
+            } 
+        },
+        {
+            type: 'input',
+            name: 'office',
+            message: 'Please enter the managers office number (Required)',
+            validate: officeInput => {
+                if (officeInput) {
+                    return true;
+                } else {
+                    console.log ('Please enter an office number');
+                    return false;
+                }
+            }
+        }
+    ])
+    .then(inputManager => {
+        const { name, id, email, office } = inputManager;
+        const manager = new Manager (name, id, email, office);
+        teamArray.push(manager);
+        console.log(manager);
+    })
+};
